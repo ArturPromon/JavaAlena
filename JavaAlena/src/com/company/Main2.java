50eab44fb5c8d9847c50ec455af6ef7eefcff59d
ArturPromon <artur.shovkunenko@gmail.com>
2022-11-15 20:08:36 +0200

    Types of variables

---
+++ JavaAlena/src/com/company/Main2.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main2 {
+    public static void main(String[] args){
+        int number = 100000;
+        //byte [-128, 128)
+        byte number2 = 127;
+        float number3 = 5.5f;
+        double number4 = 6.5d;
+        char symbol = '+';
+        String str = "Hello";
+        boolean isTrue= true;
+
+        System.out.println(number);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(symbol);
+        System.out.println(str);
+        System.out.println(isTrue);
+    }
+}
