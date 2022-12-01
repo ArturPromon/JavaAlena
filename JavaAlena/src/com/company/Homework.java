0f1cbccd458b59640deac558dbad83a965f2a42e
ArturPromon <artur.shovkunenko@gmail.com>
2022-12-01 18:07:04 +0200

    test

---
+++ JavaAlena/src/com/company/Homework.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Homework {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5};
+        int[] b = new int[a.length];
+        int count = 0;
+        for(int i = a.length - 1; i > -1; i--){
+            b[count] = a[i];
+
+            count++;
+        }
+        System.out.println(Arrays.toString(b));
+    }
+}
