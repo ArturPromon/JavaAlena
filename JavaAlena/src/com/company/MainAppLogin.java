9f348b2d75cc0227c6a817b3d2f879696e85b1e1
ArturPromon <artur.shovkunenko@gmail.com>
2023-01-10 20:20:24 +0200

    exam

--- JavaAlena/src/com/company/MainAppLogin.java
+++ JavaAlena/src/com/company/MainAppLogin.java
@@ -2,7 +2,7 @@ package com.company;
 
 import javax.swing.*;
 
-public class MainAppLogin {
+public class MainAppLogin  {
     public static void main(String[] args) {
         MainAppLogin2 app = new MainAppLogin2();
         app.setVisible(true);
